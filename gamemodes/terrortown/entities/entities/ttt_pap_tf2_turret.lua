0092f182a6bc87cdebc48394ddad6d6c44f2a283
The Stig <thestig294@gmail.com>
2025-04-07 21:22:17 +0930

    Finally fixed the turret shooting...

--- gamemodes/terrortown/entities/entities/ttt_pap_tf2_turret.lua
+++ gamemodes/terrortown/entities/entities/ttt_pap_tf2_turret.lua
@@ -14,6 +14,7 @@ ENT.Delay = 0.2
 ENT.Angle = 45
 ENT.Range = 500
 ENT.Range = ENT.Range * ENT.Range
+ENT.SearchDelay = 0.5
 
 function ENT:SetupDataTables()
     self:NetworkVar("Bool", "Idle")
@@ -36,38 +37,13 @@ function ENT:Initialize()
     self:SetAttacking(false)
     self:SetNextFire(CurTime())
     self:SetOriginalAngles(self:GetAngles())
-    -- timer.Create("TF2TurretTest", 1, 0, function()
-    --     if not IsValid(self) then return end
-    --     if not self:GetIdle() and self:GetIdleTimer() <= CurTime() and not self:GetFullyPlaced() then
-    --         self:SetModel("models/buildables/sentry1.mdl")
-    --         self:SetFullyPlaced(true)
-    --         self:ResetSequence("aim_nat")
-    --         self:SetIdle(true)
-    --     end
-    --     if self:GetFullyPlaced() then
-    --         if self:GetIdle() then
-    --             self:FindTarget()
-    --         elseif self:IsValidTarget() then
-    --             self:Attack()
-    --         else
-    --             self:Reset()
-    --         end
-    --     end
-    -- end)
 end
 
 function ENT:IsValidTarget()
-    -- print("Is valid target called")
     local target = self:GetTarget()
-    -- print("Is valid:", IsValid(target), "Is player:", target:IsPlayer(), "Alive:", target:Alive(), "IsSpec:", target:IsSpec())
     if not IsValid(target) or not target:IsPlayer() or not target:Alive() or target:IsSpec() then return false end
-    -- local pos = self:GetPos()
-    -- local targetPos = target:GetPos()
-    -- print("Target Pos:", targetPos)
-    -- print("Target within distance:", targetPos:DistToSqr(pos) < self.Range)
-    -- return targetPos:DistToSqr(pos) < self.Range
 
-    return target:GetPos():DistToSqr(self:GetPos()) < self.Range
+    return (CLIENT or self:Visible(target)) and target:GetPos():DistToSqr(self:GetPos()) < self.Range
 end
 
 function ENT:Think()
@@ -94,7 +70,9 @@ function ENT:Think()
 end
 
 function ENT:FindTarget()
-    -- print("Calling FindTarget")
+    if self:GetIdleTimer() > CurTime() then return end
+    self:SetIdleTimer(CurTime() + self.SearchDelay)
+
     for _, ent in ipairs(ents.FindInCone(self:GetPos(), self:GetForward(), self.Range, math.cos(math.rad(self.Angle)))) do
         if IsValid(ent) and ent:IsPlayer() and ent:Alive() and not ent:IsSpec() then
             self:SetTarget(ent)
@@ -106,7 +84,6 @@ end
 local angleOffset = Angle(0, 90, 80)
 
 function ENT:Attack()
-    -- print("Calling Attack")
     local target = self:GetTarget()
 
     if not self:GetAttacking() then
@@ -140,7 +117,6 @@ function ENT:Attack()
 end
 
 function ENT:Reset()
-    -- print("Calling Reset")
     self:ResetSequence("aim_nat")
     self:SetAttacking(false)
     self:SetTarget(NULL)
