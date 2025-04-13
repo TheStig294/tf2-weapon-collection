e6a45d0eda578e4a5633c1f2c3406dd7ea2a8b43
The Stig <thestig294@gmail.com>
2025-04-13 13:13:36 +0930

    Added upgrade for the caber

--- lua/pack_a_punch/upgrades/the_boomstick.lua
+++ lua/pack_a_punch/upgrades/the_boomstick.lua
@@ -6,6 +6,29 @@ UPGRADE.desc = "No self-damage, infinite explosions!"
 UPGRADE.noSound = true
 
 function UPGRADE:Apply(SWEP)
+    local timername = "TTTPAPTheBoomstick" .. SWEP:EntIndex()
+
+    timer.Create(timername, 1, 0, function()
+        if not IsValid(SWEP) then
+            timer.Remove(timername)
+
+            return
+        end
+
+        SWEP:SetExploded(false)
+
+        if IsValid(self.v_model) then
+            self.v_model:Remove()
+            self.SwitchedViewModel = false
+        end
+    end)
+
+    self:AddHook("PlayerShouldTakeDamage", function(ply, attacker)
+        if not IsValid(ply) or not IsValid(attacker) then return end
+        local wep = attacker:GetActiveWeapon()
+        if not IsValid(wep) then return end
+        if self:IsValidUpgrade(wep) and ply == attacker then return false end
+    end)
 end
 
 TTTPAP:Register(UPGRADE)
