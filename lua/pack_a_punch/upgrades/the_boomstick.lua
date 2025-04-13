beb56c256c02f290c1862c0bb43f4864e373b052
The Stig <thestig294@gmail.com>
2025-04-13 13:05:54 +0930

    Added the skeleton of the caber upgrade

---
+++ lua/pack_a_punch/upgrades/the_boomstick.lua
@@ -0,0 +1,11 @@
+local UPGRADE = {}
+UPGRADE.id = "the_boomstick"
+UPGRADE.class = "weapon_ttt_tf2_caber"
+UPGRADE.name = "The Boomstick"
+UPGRADE.desc = "No self-damage, infinite explosions!"
+UPGRADE.noSound = true
+
+function UPGRADE:Apply(SWEP)
+end
+
+TTTPAP:Register(UPGRADE)
