bd2ceb6174b246b9d0a034b6a66594c2e689eb65
The Stig <thestig294@gmail.com>
2025-04-18 16:55:05 +0930

    Added a WIP Scattergun upgrade

---
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_tf2_forceanature.lua
@@ -0,0 +1,190 @@
+SWEP.PrintName = "Force-A-Nature"
+SWEP.Author = ""
+SWEP.Contact = ""
+SWEP.Purpose = ""
+SWEP.Instructions = ""
+SWEP.Category = "Team Fortress 2"
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.AdminOnly = false
+SWEP.ViewModelFOV = 60
+SWEP.ViewModel = "models/weapons/c_models/c_scout_arms.mdl"
+SWEP.WorldModel = "models/weapons/c_models/c_double_barrel.mdl"
+SWEP.ViewModelFlip = false
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = false
+SWEP.ViewModelBoneMods = {}
+SWEP.AutoSwitchTo = false
+SWEP.AutoSwitchFrom = false
+SWEP.Slot = 0
+SWEP.SlotPos = 0
+SWEP.UseHands = true
+SWEP.HoldType = "shotgun"
+SWEP.FiresUnderwater = true
+SWEP.DrawCrosshair = false
+SWEP.DrawAmmo = false
+SWEP.Base = "weapon_tttbase"
+SWEP.Kind = WEAPON_HEAVY
+SWEP.Slot = 2
+SWEP.AutoSpawnable = false
+
+if CLIENT then
+    SWEP.EquipMenuData = {
+        type = "item_weapon",
+        desc = "A double barrel that launches players back!"
+    }
+
+    SWEP.Icon = "vgui/ttt/weapon_ttt_tf2_forceanature.png"
+end
+
+SWEP.Primary.Sound = Sound("weapons/scatter_gun_double_shoot.wav")
+SWEP.Primary.Damage = 9.45
+SWEP.Primary.ClipSize = 2
+SWEP.Primary.Ammo = "Buckshot"
+SWEP.AmmoEnt = "item_box_buckshot_ttt"
+SWEP.WeaponID = AMMO_SHOTGUN
+SWEP.Primary.DefaultClip = 2
+SWEP.Primary.Spread = 0.3
+SWEP.Primary.NumberofShots = 12
+SWEP.Primary.Automatic = true
+SWEP.Primary.Recoil = 0
+SWEP.Primary.Delay = 0.3125
+SWEP.Primary.Force = 0.3
+
+function SWEP:Deploy()
+    local owner = self:GetOwner()
+    if not IsValid(owner) then return end
+    local vm = owner:GetViewModel()
+    if not IsValid(vm) then return end
+    vm:SendViewModelMatchingSequence(vm:LookupSequence("db_draw"))
+    self:SetNextPrimaryFire(CurTime() + 0.5)
+    self:SetNextSecondaryFire(CurTime() + 0.5)
+    self.Attack = 0
+    self.AttackTimer = CurTime()
+    self.Idle = 0
+    self.IdleTimer = CurTime() + owner:GetViewModel():SequenceDuration()
+    self.Reloading = 0
+    self.ReloadingTimer = CurTime()
+
+    return self.BaseClass.Deploy(self)
+end
+
+function SWEP:PrimaryAttack()
+    if self:Clip1() <= 0 then
+        self:Reload()
+
+        return
+    end
+
+    local owner = self:GetOwner()
+    if not IsValid(owner) then return end
+    local vm = owner:GetViewModel()
+    vm:SendViewModelMatchingSequence(vm:LookupSequence("db_fire"))
+    local bullet = {}
+    bullet.Num = self.Primary.NumberofShots
+    bullet.Src = owner:GetShootPos()
+    bullet.Dir = owner:GetAimVector()
+    bullet.Spread = Vector(self.Primary.Spread * 0.1, self.Primary.Spread * 0.1, 0)
+    bullet.Tracer = 1
+    bullet.Force = self.Primary.Force
+    bullet.Damage = self.Primary.Damage
+    bullet.AmmoType = self.Primary.Ammo
+    self:ShootEffects()
+    owner:FireBullets(bullet)
+    self:EmitSound(self.Primary.Sound)
+    owner:MuzzleFlash()
+    self:TakePrimaryAmmo(1)
+    self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
+    self.ReloadingTimer = CurTime() + 1
+end
+
+function SWEP:Reload()
+    if self:Ammo1() <= 0 then return end
+    local owner = self:GetOwner()
+    if not IsValid(owner) then return end
+    local vm = owner:GetViewModel()
+    if not IsValid(vm) then return end
+    vm:SendViewModelMatchingSequence(vm:LookupSequence("db_reload"))
+    owner:SetAnimation(PLAYER_RELOAD)
+    self:SetNextPrimaryFire(CurTime() + vm:SequenceDuration())
+    self:SetNextSecondaryFire(CurTime() + vm:SequenceDuration())
+    self.Reloading = 1
+    self.ReloadingTimer = CurTime() + vm:SequenceDuration()
+    self.Idle = 0
+    self.IdleTimer = CurTime() + vm:SequenceDuration()
+end
+
+function SWEP:Think()
+    local owner = self:GetOwner()
+    if not IsValid(owner) then return end
+    local vm = owner:GetViewModel()
+    if not IsValid(vm) then return end
+
+    if self.Idle == 0 and self.IdleTimer <= CurTime() then
+        if SERVER then
+            vm:SetSequence(vm:LookupSequence("db_idle"))
+        end
+
+        self.Idle = 1
+
+        if self.Reloading == 1 then
+            local reloadCount = math.min(self.Primary.ClipSize - self:Clip1(), self:Ammo1())
+            owner:RemoveAmmo(reloadCount, self:GetPrimaryAmmoType())
+            self:SetClip1(self:Clip1() + reloadCount)
+            self.Reloading = 0
+        end
+    end
+
+    if self.Reloading == 0 and self.ReloadingTimer <= CurTime() and self:Clip1() < self:GetMaxClip1() then
+        self:Reload()
+    end
+end
+
+if CLIENT then
+    function SWEP:ViewModelDrawn(vm)
+        local owner = self:GetOwner()
+        if not IsValid(owner) then return end
+        if not IsValid(vm) then return end
+
+        if not IsValid(self.v_model) then
+            self.v_model = ClientsideModel("models/weapons/c_models/c_double_barrel.mdl", RENDERGROUP_VIEWMODEL)
+        end
+
+        self.v_model:SetPos(vm:GetPos())
+        self.v_model:SetAngles(vm:GetAngles())
+        self.v_model:AddEffects(EF_BONEMERGE)
+        self.v_model:SetNoDraw(true)
+        self.v_model:SetParent(vm)
+        self.v_model:DrawModel()
+    end
+
+    local w_model = ClientsideModel(SWEP.WorldModel)
+    w_model:SetNoDraw(true)
+    local offsetvec = Vector(2.596, 0, 0)
+    local offsetang = Angle(180, 90, 0)
+
+    function SWEP:DrawWorldModel(flags)
+        local owner = self:GetOwner()
+
+        if not IsValid(owner) then
+            self:DrawModel(flags)
+
+            return
+        end
+
+        local boneid = owner:LookupBone("ValveBiped.Bip01_R_Hand")
+        if not boneid then return end
+        local matrix = owner:GetBoneMatrix(boneid)
+        if not matrix then return end
+        local newpos, newang = LocalToWorld(offsetvec, offsetang, matrix:GetTranslation(), matrix:GetAngles())
+
+        if not IsValid(self.w_model) then
+            self.w_model = w_model
+        end
+
+        self.w_model:SetPos(newpos)
+        self.w_model:SetAngles(newang)
+        self.w_model:SetupBones()
+        self.w_model:DrawModel()
+    end
+end
---
+++ lua/pack_a_punch/upgrades/force_a_nature.lua
@@ -0,0 +1,9 @@
+local UPGRADE = {}
+UPGRADE.id = "force_a_nature"
+UPGRADE.class = "weapon_ttt_tf2_scattergun"
+UPGRADE.name = "Force-A-Nature"
+UPGRADE.desc = "A double barrel that launches players back!"
+UPGRADE.noSound = true
+UPGRADE.noCamo = true
+UPGRADE.newClass = "weapon_ttt_tf2_forceanature"
+TTTPAP:Register(UPGRADE)
