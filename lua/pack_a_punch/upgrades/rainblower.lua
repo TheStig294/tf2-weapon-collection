ceb0dd00cae9aaebf98d6cbc6b6169782162bc9d
The Stig <thestig294@gmail.com>
2025-04-19 14:27:44 +0930

    Increased the damage of the Rainblower

--- lua/pack_a_punch/upgrades/rainblower.lua
+++ lua/pack_a_punch/upgrades/rainblower.lua
@@ -2,10 +2,11 @@ local UPGRADE = {}
 UPGRADE.id = "rainblower"
 UPGRADE.class = "weapon_ttt_tf2_flamethrower"
 UPGRADE.name = "Rainblower"
-UPGRADE.desc = "Double range and ammo... Shoots rainbows!"
+UPGRADE.desc = "Increased damage, range and ammo... Shoots rainbows!"
 UPGRADE.noSound = true
 UPGRADE.newClass = "weapon_ttt_tf2_rainblower"
 UPGRADE.ammoMult = 2
+UPGRADE.damageMult = 1.5
 
 function UPGRADE:Apply(SWEP)
     SWEP.Primary.Range = SWEP.Primary.Range * 2
