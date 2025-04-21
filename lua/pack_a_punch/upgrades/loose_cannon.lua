93d1d7b84246744ec74951ae9928cb554023912f
The Stig <thestig294@gmail.com>
2025-04-21 19:03:36 +0930

    Removed camo from grenade launcher upgrade

--- lua/pack_a_punch/upgrades/loose_cannon.lua
+++ lua/pack_a_punch/upgrades/loose_cannon.lua
@@ -4,7 +4,6 @@ UPGRADE.class = "weapon_ttt_tf2_grenadelauncher"
 UPGRADE.name = "Loose Cannon"
 UPGRADE.desc = "Hold fire to charge, shoots high-damage cannonballs!"
 UPGRADE.noSound = true
-UPGRADE.noCamo = true
 UPGRADE.newClass = "weapon_ttt_tf2_loosecannon"
 
 function UPGRADE:Apply(SWEP)
