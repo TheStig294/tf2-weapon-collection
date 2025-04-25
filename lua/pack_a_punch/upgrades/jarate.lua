e22ac7e8bec4a53ff52f13162d6d273ea3ac388a
The Stig <thestig294@gmail.com>
2025-04-26 00:05:24 +0930

    Fixed upgrade description

--- lua/pack_a_punch/upgrades/jarate.lua
+++ lua/pack_a_punch/upgrades/jarate.lua
@@ -2,7 +2,7 @@ local UPGRADE = {}
 UPGRADE.id = "jarate"
 UPGRADE.class = "weapon_ttt_tf2_smg"
 UPGRADE.name = "Jarate"
-UPGRADE.desc = "Throw at players to make them temporarily take more damage!"
+UPGRADE.desc = "Throw at players to make them temporarily have 1 HP!"
 UPGRADE.noSound = true
 UPGRADE.newClass = "weapon_ttt_tf2_jarate"
 
