1e365436833f5862b8276ab4ce540d6764e7c5f4
The Stig <thestig294@gmail.com>
2025-04-25 22:00:10 +0930

    Weapons with unique models for their upgrades now no longer have the PaP camo applied to their viewmodels

--- lua/pack_a_punch/upgrades/loose_cannon.lua
+++ lua/pack_a_punch/upgrades/loose_cannon.lua
@@ -9,12 +9,6 @@ UPGRADE.newClass = "weapon_ttt_tf2_loosecannon"
 function UPGRADE:Apply(SWEP)
     self:SetClip(SWEP, 1)
 
-    self:AddToHook(SWEP, "ViewModelDrawn", function()
-        if IsValid(SWEP.v_model) and SWEP.v_model:GetMaterial() ~= TTTPAP.camo then
-            SWEP.v_model:SetPAPCamo()
-        end
-    end)
-
     self:AddToHook(SWEP, "DrawWorldModel", function()
         if IsValid(SWEP.w_model) and SWEP.w_model:GetMaterial() ~= TTTPAP.camo then
             SWEP.w_model:SetPAPCamo()
