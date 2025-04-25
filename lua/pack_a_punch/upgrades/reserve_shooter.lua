55e2fba8c710557b46d7cd37c54bb814e96f0399
The Stig <thestig294@gmail.com>
2025-04-25 17:04:31 +0930

    Added an upgrade for the Shotgun

---
+++ lua/pack_a_punch/upgrades/reserve_shooter.lua
@@ -0,0 +1,28 @@
+local UPGRADE = {}
+UPGRADE.id = "reserve_shooter"
+UPGRADE.class = "weapon_ttt_tf2_shotgun"
+UPGRADE.name = "Reserve Shooter"
+UPGRADE.desc = "1-shot kills anyone in the air!"
+UPGRADE.noSound = true
+
+function UPGRADE:Apply(SWEP)
+    SWEP.Primary.Sound = "weapons/reserve_shooter_0" .. math.random(4) .. ".wav"
+
+    self:AddToHook(SWEP, "PrimaryAttack", function()
+        SWEP.Primary.Sound = "weapons/reserve_shooter_0" .. math.random(4) .. ".wav"
+    end)
+
+    self:AddHook("EntityTakeDamage", function(ent, dmg)
+        if not IsValid(ent) or IsValid(ent:GetGroundEntity()) or ent:GetGroundEntity():IsWorld() then return end
+        local attacker = dmg:GetAttacker()
+        if not IsValid(attacker) then return end
+        local inflictor = dmg:GetInflictor()
+
+        if self:IsValidUpgrade(inflictor) then
+            dmg:SetDamage(10000)
+            attacker:EmitSound("weapons/reserve_shooter_0" .. math.random(4) .. "_crit.wav")
+        end
+    end)
+end
+
+TTTPAP:Register(UPGRADE)
