9b27f3d650abf9eafb3b873aeaf27260eb7478a1
The Stig <thestig294@gmail.com>
2025-04-25 18:00:34 +0930

    Added an upgrade for the Sniper Rifle

---
+++ lua/pack_a_punch/upgrades/awper_hand.lua
@@ -0,0 +1,31 @@
+local UPGRADE = {}
+UPGRADE.id = "awper_hand"
+UPGRADE.class = "weapon_ttt_tf2_sniper"
+UPGRADE.name = "AWPer Hand"
+UPGRADE.desc = "Full-charge headshots always kill!"
+UPGRADE.noSound = true
+
+function UPGRADE:Apply(SWEP)
+    self:SetClip(SWEP, 3)
+
+    self:AddHook("ScalePlayerDamage", function(victim, hitgroup, dmg)
+        if hitgroup ~= HITGROUP_HEAD then return end
+        local inflictor = dmg:GetInflictor()
+        if not self:IsValidUpgrade(inflictor) or inflictor.Primary.Damage < inflictor.Primary.FullChargeDamage then return end
+        dmg:SetDamage(10000)
+        local attacker = dmg:GetAttacker()
+
+        if IsValid(attacker) then
+            attacker:EmitSound("player/crit_hit.wav")
+        end
+
+        if CLIENT then return end
+
+        timer.Simple(0.1, function()
+            if not self:IsAlivePlayer(victim) then return end
+            victim:Kill()
+        end)
+    end)
+end
+
+TTTPAP:Register(UPGRADE)
