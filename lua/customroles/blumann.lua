dc3a763813b18301586fb12a230f0b44b9a8cfc4
The Stig <thestig294@gmail.com>
2025-04-27 15:50:51 +0930

    Cleanup BLU/REDmann roles

--- lua/customroles/blumann.lua
+++ lua/customroles/blumann.lua
@@ -11,14 +11,12 @@ Instead of buying items, you choose a TF2 class to play as! (Press ',')
 Press {menukey} to receive your special equipment]]
 ROLE.shortdesc = "Chooses a TF2 class' abilities instead of buying equipment"
 ROLE.team = ROLE_TEAM_DETECTIVE
-ROLE.shop = {}
+
+ROLE.shop = {"Change TF2 Class"}
+
 ROLE.loadout = {}
-ROLE.startingcredits = 1
 ROLE.startinghealth = 100
 ROLE.maxhealth = 100
-ROLE.isactive = nil
-ROLE.selectionpredicate = nil
-ROLE.shouldactlikejester = nil
 ROLE.translations = {}
 ROLE.convars = {}
 RegisterRole(ROLE)
@@ -32,8 +30,8 @@ if CLIENT then
     hook.Add("TTTTutorialRoleText", "BLUMann_TTTTutorialRoleText", function(role, _)
         if role == ROLE_BLUMANN then
             local roleColor = GetRoleTeamColor(ROLE_TEAM_DETECTIVE)
-            local traitorColor = GetRoleTeamColor(ROLE_TEAM_TRAITOR)
-            local html = "The " .. ROLE_STRINGS[ROLE_BLUMANN] .. " is a <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> role who chooses a class to play as from Team Fortress 2, and gains their abilities! This is instead of choosing items in a buy menu.<br>Press the comma [,] key to bring up the class selection screen.<br>To change roles again, they can buy the 'Class Changer' item in their buy menu; however, <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>this costs a credit!</span><br>This role is the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> equivalent to the " .. ROLE_STRINGS[ROLE_BLUMANN] .. ", which works exactly the same, except it is a <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>detective</span> role."
+            local counterpartColour = GetRoleTeamColor(ROLE_TEAM_TRAITOR)
+            local html = "The " .. ROLE_STRINGS[ROLE_BLUMANN] .. " is a <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> role who chooses a class to play as from Team Fortress 2, and gains their abilities! This is instead of choosing items in a buy menu.<br>Press the comma [,] key to bring up the class selection screen.<br>To change roles again, they can buy the 'Class Changer' item in their buy menu; however, <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>this costs a credit!</span><br>This role is the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> equivalent to the " .. ROLE_STRINGS[ROLE_BLUMANN] .. ", which works exactly the same, except it is a <span style='color: rgb(" .. counterpartColour.r .. ", " .. counterpartColour.g .. ", " .. counterpartColour.b .. ")'>detective</span> role."
 
             return html
         end
--- lua/customroles/redmann.lua
+++ lua/customroles/redmann.lua
@@ -11,14 +11,12 @@ Instead of buying items, you choose a TF2 class to play as! (Press ',')
 Press {menukey} to receive your special equipment]]
 ROLE.shortdesc = "Chooses a TF2 class' abilities instead of buying equipment"
 ROLE.team = ROLE_TEAM_TRAITOR
-ROLE.shop = {}
+
+ROLE.shop = {"Change TF2 Class"}
+
 ROLE.loadout = {}
-ROLE.startingcredits = 1
 ROLE.startinghealth = 100
 ROLE.maxhealth = 100
-ROLE.isactive = nil
-ROLE.selectionpredicate = nil
-ROLE.shouldactlikejester = nil
 ROLE.translations = {}
 ROLE.convars = {}
 RegisterRole(ROLE)
@@ -50,8 +48,8 @@ if CLIENT then
     hook.Add("TTTTutorialRoleText", "REDMann_TTTTutorialRoleText", function(role, _)
         if role == ROLE_REDMANN then
             local roleColor = GetRoleTeamColor(ROLE_TEAM_TRAITOR)
-            local detectiveColor = GetRoleTeamColor(ROLE_TEAM_DETECTIVE)
-            local html = "The " .. ROLE_STRINGS[ROLE_REDMANN] .. " is a <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> role who chooses a class to play as from Team Fortress 2, and gains their abilities! This is instead of choosing items in a buy menu.<br>Press the comma [,] key to bring up the class selection screen.<br>To change roles again, they can buy the 'Class Changer' item in their buy menu; however, <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>this costs a credit!</span><br>This role is the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> equivalent to the " .. ROLE_STRINGS[ROLE_BLUMANN] .. ", which works exactly the same, except it is a <span style='color: rgb(" .. detectiveColor.r .. ", " .. detectiveColor.g .. ", " .. detectiveColor.b .. ")'>detective</span> role."
+            local counterpartColour = GetRoleTeamColor(ROLE_TEAM_DETECTIVE)
+            local html = "The " .. ROLE_STRINGS[ROLE_REDMANN] .. " is a <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> role who chooses a class to play as from Team Fortress 2, and gains their abilities! This is instead of choosing items in a buy menu.<br>Press the comma [,] key to bring up the class selection screen.<br>To change roles again, they can buy the 'Class Changer' item in their buy menu; however, <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>this costs a credit!</span><br>This role is the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor</span> equivalent to the " .. ROLE_STRINGS[ROLE_BLUMANN] .. ", which works exactly the same, except it is a <span style='color: rgb(" .. counterpartColour.r .. ", " .. counterpartColour.g .. ", " .. counterpartColour.b .. ")'>detective</span> role."
 
             return html
         end
