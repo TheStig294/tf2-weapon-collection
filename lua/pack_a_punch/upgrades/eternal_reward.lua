e94bb3877627a8daf92a40d992b08f5df11267dd
The Stig <thestig294@gmail.com>
2025-06-01 14:29:01 +0930

    Added the PaP camo to all upgraded weapons missing it in their viewmodels

--- lua/pack_a_punch/upgrades/eternal_reward.lua
+++ lua/pack_a_punch/upgrades/eternal_reward.lua
@@ -7,6 +7,12 @@ UPGRADE.noSound = true
 UPGRADE.newClass = "weapon_ttt_tf2_eternalreward"
 
 function UPGRADE:Apply(SWEP)
+    self:AddToHook(SWEP, "ViewModelDrawn", function()
+        if IsValid(SWEP.v_model) and SWEP.v_model:GetMaterial() ~= TTTPAP.camo then
+            SWEP.v_model:SetPAPCamo()
+        end
+    end)
+
     self:AddToHook(SWEP, "DrawWorldModel", function()
         if IsValid(SWEP.w_model) and SWEP.w_model:GetMaterial() ~= TTTPAP.camo then
             SWEP.w_model:SetPAPCamo()
--- lua/pack_a_punch/upgrades/shortstop.lua
+++ lua/pack_a_punch/upgrades/shortstop.lua
@@ -9,6 +9,12 @@ UPGRADE.newClass = "weapon_ttt_tf2_shortstop"
 function UPGRADE:Apply(SWEP)
     self:SetClip(SWEP, 4)
 
+    self:AddToHook(SWEP, "ViewModelDrawn", function()
+        if IsValid(SWEP.v_model) and SWEP.v_model:GetMaterial() ~= TTTPAP.camo then
+            SWEP.v_model:SetPAPCamo()
+        end
+    end)
+
     self:AddToHook(SWEP, "DrawWorldModel", function()
         if IsValid(SWEP.w_model) and SWEP.w_model:GetMaterial() ~= TTTPAP.camo then
             SWEP.w_model:SetPAPCamo()
